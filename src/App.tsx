82f09c39645ea8ad3aa0e7f65933d56364f072c3
devmvrborges <mvrborges0@gmail.com>
2022-12-16 09:35:08 -0300

    add homepage hello world

--- src/App.tsx
+++ src/App.tsx
@@ -7,18 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>TESTE MAU</h1>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+     
       </header>
     </div>
   );
