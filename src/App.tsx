8a325be89db94092a00668829b2ad5b370ccef09
devmvrborges <mvrborges0@gmail.com>
2022-12-16 09:05:41 -0300

    initial home page

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,19 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>TESTE MAAAAAAU</h1>
+        <h1>TESTE MAU</h1>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </header>
     </div>
   );
