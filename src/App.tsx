1999b8ef1cfba3ea190a106299c4aae04afc7d30
devmvrborges <mvrborges0@gmail.com>
2022-12-16 09:37:40 -0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>TESTE MAU</h1>
-     
+        <img src={logo} className="App-logo" alt="logo" />
+        
       </header>
     </div>
   );
