98d0d648ccd180e1ecffbd5a7d7b4b5b7186f2e9
devmvrborges <mvrborges0@gmail.com>
2022-12-17 20:14:16 -0300

    add tabs

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import explorer_icon from './explorer_icon.svg'
 import git_icon from './git_icon.svg'
+import linkedin_icon from './linkedin_icon.svg'
+import pdf_icon from './pdf_icon.svg'
+import md_icon from './md_icon.svg'
+import cs_icon from './cs_icon.svg'
+import config_icon from './config_icon.svg'
 import './App.css';
+
 
 function App() {
   return (
+    
     <div className="App">
       <header className="App-header">
 
@@ -12,23 +19,51 @@ function App() {
           <div className="vs-code-top"> 
             <div className="left">☕</div>
             <div className="center">mauricio-borges-dev</div>
+           
           </div>
           <div className="vs-code-content">
             <div className="vs-code-left-tools">
               <img src={explorer_icon} className='vs-icon' alt="logo" />
-            <a href='www.google.com' target="_blank" rel="noreferrer">
-              <img src={git_icon} className='vs-icon' alt="logo" />
-            </a>
-            </div>
+              <a href="https://github.com/devmvrborges/" target='_blank' rel="noopener">
+                <img src={git_icon} className='vs-icon' alt="logo" />
+              </a>
+              <a href="https://www.linkedin.com/in/mvrborges/" target='_blank' rel="noopener">
+                <img src={linkedin_icon} className='vs-icon' alt="logo" />
+              </a>
           </div>
+          
+<section>
+  <div className="vs-code-tabs">
+    <img src={md_icon} className='icon-white' alt="logo" />
+    about.md</div>
+</section>
+
+<section>
+  <div className="vs-code-tabs">
+    <img src={pdf_icon} className='tab-icon' alt="logo" />
+    resume.pdf</div>
+</section>
+
+<section>
+  <div className="vs-code-tabs">
+    <img src={cs_icon} className='icon-white' alt="logo" />
+    projects.sln</div>
+</section>
+
+<section>
+  <div className="vs-code-tabs">
+    <img src={config_icon} className='icon-white' alt="logo" />
+    contact.config</div>
+</section>
+
         </div>
+        </div>
+
         
-      </header>
+              </header>
     </div>
-  );
+ );
 }
-
-
 
 
 
