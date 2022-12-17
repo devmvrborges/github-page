eabb41573ec6f1ee01e463cf026b2055468cafaf
devmvrborges <mvrborges0@gmail.com>
2022-12-17 17:47:59 -0300

    add left tools vscode

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import explorer_icon from './explorer_icon.svg'
+import git_icon from './git_icon.svg'
 import './App.css';
 
 function App() {
@@ -11,10 +13,23 @@ function App() {
             <div className="left">☕</div>
             <div className="center">mauricio-borges-dev</div>
           </div>
+          <div className="vs-code-content">
+            <div className="vs-code-left-tools">
+              <img src={explorer_icon} className='vs-icon' alt="logo" />
+            <a href='www.google.com' target="_blank" rel="noreferrer">
+              <img src={git_icon} className='vs-icon' alt="logo" />
+            </a>
+            </div>
+          </div>
         </div>
+        
       </header>
     </div>
   );
 }
+
+
+
+
 
 export default App;
