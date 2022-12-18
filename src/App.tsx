f5b535f2676c675540f716bf5d88e6c058d3f119
devmvrborges <mvrborges0@gmail.com>
2022-12-18 17:23:51 -0300

    add content vscode

--- src/App.tsx
+++ src/App.tsx
@@ -7,19 +7,15 @@ import md_icon from './md_icon.svg'
 import cs_icon from './cs_icon.svg'
 import config_icon from './config_icon.svg'
 import './App.css';
-
-
 function App() {
   return (
-    
     <div className="App">
       <header className="App-header">
 
         <div className="vs-code-window">
-          <div className="vs-code-top"> 
+          <div className="vs-code-top">
             <div className="left">☕</div>
             <div className="center">mauricio-borges-dev</div>
-           
           </div>
           <div className="vs-code-content">
             <div className="vs-code-left-tools">
@@ -30,35 +26,35 @@ function App() {
               <a href="https://www.linkedin.com/in/mvrborges/" rel="noopener">
                 <img src={linkedin_icon} className='vs-icon' alt="logo" />
               </a>
-          </div>
-          <div className='vs-code-tab-main'>
-<section>
-  <div className="vs-code-tabs">
-    <img src={md_icon} className='icon-white' alt="logo" />
-    <span className='span-tab'>about.md</span></div>
-
-  <div className="vs-code-tabs">
-    <img src={pdf_icon} className='tab-icon' alt="logo" />
-    <span className='span-tab'>resume.pdf</span></div>
-
-  <div className="vs-code-tabs">
-    <img src={cs_icon} className='icon-white' alt="logo" />
-    <span className='span-tab'>projects.sln</span></div>
-
-  <div className="vs-code-tabs">
-    <img src={config_icon} className='icon-white' alt="logo" />
-    <span className='span-tab'>contact.config</span></div>
-</section>
-</div>
+            </div>
+            <div className='vs-code-tab-main'>
+              <section>
+                <div className="vs-code-tabs">
+                  <img src={md_icon} className='icon-white' alt="logo" />
+                  <span className='span-tab'>about.md</span></div>
+                <div className="vs-code-tabs">
+                  <img src={pdf_icon} className='tab-icon' alt="logo" />
+                  <span className='span-tab'>resume.pdf</span></div>
+                <div className="vs-code-tabs">
+                  <img src={cs_icon} className='icon-white' alt="logo" />
+                  <span className='span-tab'>projects.sln</span></div>
+                <div className="vs-code-tabs">
+                  <img src={config_icon} className='icon-white' alt="logo" />
+                  <span className='span-tab'>contact.config</span></div>
+              </section>
+              <div className='vs-content'>What is Lorem Ipsum?
+Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. What is Lorem Ipsum?
+Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. What is Lorem Ipsum?
+Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. What is Lorem Ipsum?
+Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.What is Lorem Ipsum?
+Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.What is Lorem Ipsum?
+Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.What is Lorem Ipsum?
+Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</div>
+            </div>
+          </div>  
         </div>
-        </div>
-
-        
-              </header>
+      </header>
     </div>
- );
+  );
 }
-
-
-
 export default App;
