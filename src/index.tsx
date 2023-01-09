e57e2ddcd296da93fa3bdb7ccd1be49bbfb0a02d
devmvrborges <mvrborges0@gmail.com>
2023-01-09 16:10:06 -0300

    remove unsed vards

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { StyledEngineProvider } from '@mui/material/styles';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
