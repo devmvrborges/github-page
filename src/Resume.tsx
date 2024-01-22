05f1fd49c747ea252475b09480cc5c765c75a92b
Mauricio Borges <mvrborges0@gmail.com>
2024-01-21 22:39:44 -0300

    change layout to responsive

--- src/Resume.tsx
+++ src/Resume.tsx
@@ -1,8 +1,19 @@
 import React from 'react';
+import TypingAnimation from './TypingAnimation';
 import './styles.css';
-import { FaCopy } from 'react-icons/fa';
+import { VscCopy } from "react-icons/vsc";
+import { HiOutlineUser, HiOutlineRocketLaunch } from "react-icons/hi2";
+import { VscMortarBoard } from "react-icons/vsc";
+import { MdOutlineWorkOutline } from "react-icons/md";
+import { PiCertificate } from "react-icons/pi";
+import { FiExternalLink } from "react-icons/fi";
+import { ImLab, ImWhatsapp } from "react-icons/im";
+import { CgMail } from "react-icons/cg";
+import { TbBrandLinkedin } from "react-icons/tb";
+import { FaGithub } from "react-icons/fa";
 
 const Resume = () => {
+
   const handleCopyToClipboard = (text: string) => {
     navigator.clipboard.writeText(text)
       .then(() => {
@@ -13,24 +24,64 @@ const Resume = () => {
       });
   };
 
-  return (
-        <div className="a4-layout">
-            <div className='headContainer'>
-                <div className='headLeft'>
-                    <div className='headName'>Maurício<br/>Borges</div>
-                </div>
-                <div className='contact headRight'>
-                    <div>+55 19 9966 0 7913 <FaCopy onClick={() => handleCopyToClipboard('+55 19 9966 0 7813')}  />
-                        </div>
-                    <div>mvrborges0@gmail.com <FaCopy onClick={() => handleCopyToClipboard('mvrborges0@gmail.com')} /></div>
-                </div>
-            </div>
-            <div className='bodyContainer'>
 
-            </div>
+  const handleWhatsAppClick = () => {
+    const link = `https://wa.me/5519996607813`;
+    window.location.href = link;
+  }
+
+  return (
+    <div className="a4-layout">
+      <div className='headContainer'>
+        <div className="head headLeft">
+          <div className='headName'>
+            Maurício <br />Borges
+          </div></div>
+        <div className="headCenter"></div>
+        <div className="head headRight contact">
+          <div className='contactLines'><a href='https://wa.me/5519996607813' target='_blank'> <ImWhatsapp className='copyIcon' /></a> <span onClick={() => handleCopyToClipboard('+55 19 9966 0 7813')}> +55 19 9966 0 7913 <VscCopy className='copyIcon' /></span> </div>
+          <div className='contactLines'><CgMail /><span onClick={() => handleCopyToClipboard('mvrborges0@gmail.com')} > mvrborges0@gmail.com </span><VscCopy className='copyIcon' /></div>
+          <div className='contactLines'><a href='https://linkedin.com/in/mvrborges' target='_blank'><TbBrandLinkedin /> linkedin.com/in/mvrborges <FiExternalLink className='externalIcon' /></a></div>
+          <div className='contactLines'><a href='https://github.com/devmvrborges' target='_blank'><FaGithub /> github.com/devmvrborges <FiExternalLink className='externalIcon' /></a></div>
         </div>
+      </div>
+      <div className='bodyContainer'>
+        <div>
+        <div className='section'>
+            <div className='sectionText'> <HiOutlineUser /> Profile</div>
+            <div className='sectionContentText'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ligula ante, commodo luctus sapien ut, condimentum euismod felis. Nullam consequat nisi condimentum odio interdum, eget mollis diam porttitor. Morbi massa nunc, tincidunt sit amet bibendum a, scelerisque et sem. Ut aliquet risus enim, vel lobortis libero fermentum eu. Donec scelerisque.</div>
+          </div>
+          <div className='section'>
+            <div className='sectionText'> <MdOutlineWorkOutline /> Work experience</div>
+            <div className='sectionContentText'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ligula ante, commodo luctus sapien ut, condimentum euismod felis. Nullam consequat nisi condimentum odio interdum, eget mollis diam porttitor. Morbi massa nunc, tincidunt sit amet bibendum a, scelerisque et sem. Ut aliquet risus enim, vel lobortis libero fermentum eu. Donec scelerisque.</div>
+          </div>
+          <div className='section'>
+            <div className='sectionText'> <VscMortarBoard />Education</div>
+            <div className='sectionContentText'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ligula ante, commodo luctus sapien ut, condimentum euismod felis. Nullam consequat nisi condimentum odio interdum, eget mollis diam porttitor. Morbi massa nunc, tincidunt sit amet bibendum a, scelerisque et sem. Ut aliquet risus enim, vel lobortis libero fermentum eu. Donec scelerisque.</div>
+          </div>
+        </div>
+        <div>
+        <div>
+          <div className='section'>
+            <div className='sectionText'> <ImLab /> Skills</div>
+            <div className='sectionContentText'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ligula ante, commodo luctus sapien ut, condimentum euismod felis. Nullam consequat nisi condimentum odio interdum, eget mollis diam porttitor. Morbi massa nunc, tincidunt sit amet bibendum a, scelerisque et sem. Ut aliquet risus enim, vel lobortis libero fermentum eu. Donec scelerisque.</div>
+          </div>
+          <div className='section'>
+            <div className='sectionText'> <HiOutlineRocketLaunch /> Last projects</div>
+            <div className='sectionContentText'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ligula ante, commodo luctus sapien ut, condimentum euismod felis. Nullam consequat nisi condimentum odio interdum, eget mollis diam porttitor. Morbi massa nunc, tincidunt sit amet bibendum a, scelerisque et sem. Ut aliquet risus enim, vel lobortis libero fermentum eu. Donec scelerisque.</div>
+          </div>
+          <div className='section'>
+            <div className='sectionText'> <PiCertificate /> Certificates</div>
+            <div className='sectionContentText'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ligula ante, commodo luctus sapien ut, condimentum euismod felis. Nullam consequat nisi condimentum odio interdum, eget mollis diam porttitor. Morbi massa nunc, tincidunt sit amet bibendum a, scelerisque et sem. Ut aliquet risus enim, vel lobortis libero fermentum eu. Donec scelerisque.</div>
+          </div>
+        </div>
+        </div>
+      </div>
+
+
+       
+    </div>
   );
 };
 
 export default Resume;
-
---
+++ src/TypingAnimation.tsx
@@ -0,0 +1,66 @@
+// TypingAnimation.tsx
+import React, { useState, useEffect } from 'react';
+import styled, { keyframes } from 'styled-components';
+
+const typingAnimation = keyframes`
+  from {
+    width: 0;
+  }
+  to {
+    width: 100%;
+  }
+`;
+
+const deletingAnimation = keyframes`
+  from {
+    width: 100%;
+  }
+  to {
+    width: 0;
+  }
+`;
+
+const Wrapper = styled.div`
+  overflow: hidden;
+  white-space: nowrap;
+  margin: 0 auto;
+`;
+
+const TypingText = styled.div<{ isDeleting: boolean }>`
+  display: inline-block;
+  overflow: hidden;
+  white-space: nowrap;
+  width: 100%;
+  animation: ${(props) => (props.isDeleting ? deletingAnimation : typingAnimation)} 2s steps(40, end) forwards;
+`;
+
+interface TypingAnimationProps {
+  texts: string[];
+}
+
+const TypingAnimation: React.FC<TypingAnimationProps> = ({ texts }) => {
+  const [isVisible, setIsVisible] = useState(false);
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [isDeleting, setIsDeleting] = useState(false);
+
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      setIsVisible(true);
+      setTimeout(() => setIsDeleting(true), 2000);
+      setTimeout(() => {
+        setIsDeleting(false);
+        setCurrentIndex((prevIndex) => (prevIndex + 1) % texts.length);
+      }, 4000);
+    }, 1000);
+
+    return () => clearTimeout(timeoutId);
+  }, [currentIndex, texts]);
+
+  return (
+    <Wrapper>
+      {isVisible && <TypingText isDeleting={isDeleting}>{texts[currentIndex]}</TypingText>}
+    </Wrapper>
+  );
+};
+
+export default TypingAnimation;
