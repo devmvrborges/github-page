1529b835371ae233ed579be1e2ee46ee0348c3e1
Mauricio Borges <mvrborges0@gmail.com>
2024-01-22 19:00:50 -0300

    add contents

--- src/Resume.tsx
+++ src/Resume.tsx
@@ -3,11 +3,12 @@ import './styles.css';
 import { VscCopy } from "react-icons/vsc";
 import { HiOutlineUser, HiOutlineRocketLaunch } from "react-icons/hi2";
 import { VscMortarBoard } from "react-icons/vsc";
-import { MdOutlineWorkOutline } from "react-icons/md";
+import { MdOutlineWorkOutline, MdCalendarMonth, MdLocationPin, MdOutlineInfo } from "react-icons/md";
 import { PiCertificate } from "react-icons/pi";
 import { FiExternalLink } from "react-icons/fi";
 import { ImLab } from "react-icons/im";
 import TypingAnimation from './TypingAnimation';
+import { Tooltip } from 'react-tooltip'
 
 const Resume = () => {
 
@@ -31,47 +32,113 @@ const Resume = () => {
           </div></div>
         <div className="headCenter"></div>
         <div className="head headRight contact">
-          <div className='contactLines'  data-tip="Texto da dica" data-for="meu-tooltip">
-          <a href='https://wa.me/5519996607813' target='_blank' rel="noreferrer"> +55 19 9966 0 7913 </a> <VscCopy className='copyIcon' onClick={() => handleCopyToClipboard('+55 19 9966 0 7813')}/> </div>
-          <div className='contactLines'><span onClick={() => handleCopyToClipboard('mvrborges0@gmail.com')} > mvrborges0@gmail.com <VscCopy className='copyIcon' /> </span></div>
-          <div className='contactLines'><a href='https://linkedin.com/in/mvrborges' target='_blank' rel="noreferrer"> linkedin.com/in/mvrborges <FiExternalLink className='externalIcon' /></a></div>
-          <div className='contactLines'><a href='https://github.com/devmvrborges' target='_blank' rel="noreferrer"> github.com/devmvrborges <FiExternalLink className='externalIcon' /></a></div>
-        
-       
+          <div className='contactLines' data-tooltip-id="contact-tooltip" data-tooltip-content="Call me on whatsapp or copy to clipboard" data-tooltip-place="bottom">
+            <a href='https://wa.me/5519996607813' target='_blank' rel="noreferrer"> +55 19 9966 0 7913 </a> <VscCopy className='copyIcon' onClick={() => handleCopyToClipboard('+55 19 9966 0 7813')} />
+          </div>
+          <div className='contactLines' data-tooltip-id="contact-tooltip" data-tooltip-content="Copy to clipboard" data-tooltip-place="bottom">
+            <span onClick={() => handleCopyToClipboard('mvrborges0@gmail.com')} > mvrborges0@gmail.com <VscCopy className='copyIcon' /> </span>
+          </div>
+          <div className='contactLines' data-tooltip-id="contact-tooltip" data-tooltip-content="Visit my LinkedIn" data-tooltip-place="bottom">
+            <a href='https://linkedin.com/in/mvrborges' target='_blank' rel="noreferrer"> linkedin.com/in/mvrborges <FiExternalLink className='externalIcon' /></a>
+          </div>
+          <div className='contactLines' data-tooltip-id="contact-tooltip" data-tooltip-content="Meet my github" data-tooltip-place="bottom">
+            <a href='https://github.com/devmvrborges' target='_blank' rel="noreferrer"> github.com/devmvrborges <FiExternalLink className='externalIcon' /></a>
+          </div>
+
+
         </div>
       </div>
       <div className='bodyContainer'>
         <div>
-        <div className='section'>
+          <div className='section'>
             <div className='sectionText'> <HiOutlineUser /> Profile</div>
-            <div className='sectionContentText'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ligula ante, commodo luctus sapien ut, condimentum euismod felis. Nullam consequat nisi condimentum odio interdum, eget mollis diam porttitor. Morbi massa nunc, tincidunt sit amet bibendum a, scelerisque et sem. Ut aliquet risus enim, vel lobortis libero fermentum eu. Donec scelerisque.</div>
+            <div className='sectionContentText'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ligula ante, commodo luctus sapien ut, condimentum euismod felis. </div>
           </div>
           <div className='section'>
             <div className='sectionText'> <MdOutlineWorkOutline /> Work experience</div>
-            <div className='sectionContentText'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ligula ante, commodo luctus sapien ut, condimentum euismod felis. Nullam consequat nisi condimentum odio interdum, eget mollis diam porttitor. Morbi massa nunc, tincidunt sit amet bibendum a, scelerisque et sem. Ut aliquet risus enim, vel lobortis libero fermentum eu. Donec scelerisque.</div>
+            <div className='sectionContentText'>
+              <div className='sectionTopic'>
+                <MdCalendarMonth /> MM/YYYY - MM/YYYY <br />
+                <MdLocationPin /> Local, País <br />
+                <span className='textBold'>Nome da empresa</span>
+                <li>text 1</li>
+                <li>text 2</li>
+                <li>text 3</li>
+                <li>text 4</li>
+              </div>
+
+              <div className='sectionTopic'>
+                <MdCalendarMonth /> MM/YYYY - MM/YYYY <br />
+                <MdLocationPin /> Local, País <br />
+                <span className='textBold'>Nome da empresa</span>
+                <li>text 1</li>
+                <li>text 2</li>
+                <li>text 3</li>
+                <li>text 4</li>
+              </div>
+            </div>
           </div>
           <div className='section'>
             <div className='sectionText'> <VscMortarBoard />Education</div>
-            <div className='sectionContentText'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ligula ante, commodo luctus sapien ut, condimentum euismod felis. Nullam consequat nisi condimentum odio interdum, eget mollis diam porttitor. Morbi massa nunc, tincidunt sit amet bibendum a, scelerisque et sem. Ut aliquet risus enim, vel lobortis libero fermentum eu. Donec scelerisque.</div>
+            <div className='sectionContentText'>
+              <div className='sectionTopic'>
+                <MdCalendarMonth /> MM/YYYY - MM/YYYY <br />
+                <MdLocationPin /> Local, País <br />
+                <span className='textBold'>Nome da faculdade</span>
+                <li>text 1</li>
+                <li>text 2</li>
+                <li>text 3</li>
+                <li>text 4</li>
+              </div>
+
+              <div className='sectionTopic'>
+                <MdCalendarMonth /> MM/YYYY - MM/YYYY <br />
+                <MdLocationPin /> Local, País <br />
+                <span className='textBold'>Nome da faculdade</span>
+                <li>text 1</li>
+                <li>text 2</li>
+                <li>text 3</li>
+                <li>text 4</li>
+              </div>
+            </div>
           </div>
         </div>
         <div>
-        <div>
-          <div className='section'>
-            <div className='sectionText'> <ImLab /> Skills</div>
-            <div className='sectionContentText'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ligula ante, commodo luctus sapien ut, condimentum euismod felis. Nullam consequat nisi condimentum odio interdum, eget mollis diam porttitor. Morbi massa nunc, tincidunt sit amet bibendum a, scelerisque et sem. Ut aliquet risus enim, vel lobortis libero fermentum eu. Donec scelerisque.</div>
-          </div>
-          <div className='section'>
-            <div className='sectionText'> <HiOutlineRocketLaunch /> Last projects</div>
-            <div className='sectionContentText'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ligula ante, commodo luctus sapien ut, condimentum euismod felis. Nullam consequat nisi condimentum odio interdum, eget mollis diam porttitor. Morbi massa nunc, tincidunt sit amet bibendum a, scelerisque et sem. Ut aliquet risus enim, vel lobortis libero fermentum eu. Donec scelerisque.</div>
-          </div>
-          <div className='section'>
-            <div className='sectionText'> <PiCertificate /> Certificates</div>
-            <div className='sectionContentText'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ligula ante, commodo luctus sapien ut, condimentum euismod felis. Nullam consequat nisi condimentum odio interdum, eget mollis diam porttitor. Morbi massa nunc, tincidunt sit amet bibendum a, scelerisque et sem. Ut aliquet risus enim, vel lobortis libero fermentum eu. Donec scelerisque.</div>
+          <div>
+            <div className='section'>
+              <div className='sectionText'> <ImLab /> Skills</div>
+              <div className='sectionContentText'>
+
+                <div className='sectionTopic'>
+                  <MdOutlineInfo /> Languages <br />
+                  <div className="mini-container">
+                    <div className="left">Português</div>
+                    <div className="right">2</div>
+                    {/* <div className="left">Ingles</div>
+                    <div className="right">4</div> */}
+                    {/* <div className="left">Espanhol</div>
+                    <div className="right">5</div> */}
+                  </div>
+                </div>
+
+ 
+
+
+              </div>
+            </div>
+            <div className='section'>
+              <div className='sectionText'> <HiOutlineRocketLaunch /> Last projects</div>
+              <div className='sectionContentText'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ligula ante, commodo luctus sapien ut, condimentum euismod felis. Nullam consequat nisi condimentum odio interdum, eget mollis diam porttitor. Morbi massa nunc, tincidunt sit amet bibendum a, scelerisque et sem. Ut aliquet risus enim, vel lobortis libero fermentum eu. Donec scelerisque.</div>
+            </div>
+            <div className='section'>
+              <div className='sectionText'> <PiCertificate /> Certificates</div>
+              <div className='sectionContentText'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ligula ante, commodo luctus sapien ut, condimentum euismod felis. Nullam consequat nisi condimentum odio interdum, eget mollis diam porttitor. Morbi massa nunc, tincidunt sit amet bibendum a, scelerisque et sem. Ut aliquet risus enim, vel lobortis libero fermentum eu. Donec scelerisque.</div>
+            </div>
           </div>
         </div>
-        </div>
-      </div>      
+      </div>
+      <Tooltip id="contact-tooltip" className="tooltip-font" />
+
     </div>
   );
 };
